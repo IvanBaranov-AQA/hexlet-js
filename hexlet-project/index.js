956c0b5dae5aeb1d9f573aedc99e809a1df879cd
IvanBaranov-QA <ivan.baranov.1709@gmail.com>
2021-01-18 09:06:47 +0300

    added eslint

--- hexlet-project/index.js
+++ hexlet-project/index.js
@@ -1,17 +1,17 @@
-//console.log('Hi, Ivan Baranov');
-//import * as async from 'async';
-//import * as lodash from 'lodash';
-import _ from "lodash";
-//import * as moment from 'moment';
-console.log(_.intersection([2, 1], [2, 3]));
-console.log(_.capitalize("Hello!!!"));
-console.log(_.compact([1, 2, false, 3]));
+// console.log('Hi, Ivan Baranov');
+// import * as async from 'async';
+// import * as lodash from 'lodash';
+// import _ from 'lodash';
+// import * as moment from 'moment';
+// console.log(_.intersection([2, 1], [2, 3]));
+// console.log(_.capitalize('Hello!!!'));
+// console.log(_.compact([1, 2, false, 3]));
 // function sayHello(user) {
 //     let message = 'Hello, I am '+user;
 //     console.log(message)
 // }
 // sayHello('Ivan');
-const fn = (a, b) => {
-  const c = a + b;
-  return c;
-};
+// const fn = (a, b) => {
+//   const c = a + b;
+//   return c;
+// };
